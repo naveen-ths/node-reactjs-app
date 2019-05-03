96a1225824319d82de9a4dfd5ebae008978ccd3b
Naveen <>
2019-05-03 13:58:47 +0530

    Added client folder

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+export default class PersonList extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      profile: {country: '', firstName: '', lastName: '', gravatar: '', organization: ''}
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3001/profile`)
+      .then(res => {
+        const profile = res.data.profile;
+        this.setState({ profile });
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>First Name: { this.state.profile.firstName }</p>
+        <p>Last Name: { this.state.profile.lastName }</p>
+        <p>Country: { this.state.profile.country }</p>
+        <p>Gravatar: { this.state.profile.gravatar }</p>
+        <p>Organization: { this.state.profile.organization }</p>
+      </div>
+    )
+  }
+}
